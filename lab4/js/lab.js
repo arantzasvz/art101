e9737b9a4d1752247bbb7d3c99073833565bd5db
arantzasvz <asvilchi@ucsc.edu>
2022-10-12 19:29:26 -0700

    push

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
