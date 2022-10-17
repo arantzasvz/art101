c3cc249ec26acebfd0eb83b18e37c0be5f121c61
arantzasvz <asvilchi@ucsc.edu>
2022-10-17 15:59:59 -0700

    java

---
+++ lab5/js/lab.js
@@ -0,0 +1,25 @@
+/*
+ * Author: Arantza Vilchis <asvilchi@ucsc.edu>
+ * Created: 18 October
+ * License: Public Domain
+ */
+
+console.log
+
+// Define Variables
+make = "Honda";
+model = "CRV";
+color = "Black";
+year = 2000;
+
+// calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + Honda + "<br>");
+document.writeln("Model: " + CRV + "<br>");
+document.writeln("Color: " + Black + "<br>");
+document.writeln("Year: " + 2000 + "<br>");
+document.writeln("Age: " + 22 + " years<br>");
+
+<script type="text/javascript" src="js/lab5.js"></script>
