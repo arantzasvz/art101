5f2f833bbc448e5014e7d48e0a2608a5c649a502
arantzasvz <asvilchi@ucsc.edu>
2022-11-08 13:20:02 -0800

    hey

--- lab10.5/js/lab.js
+++ lab10.5/js/lab.js
@@ -1,188 +1,78 @@
 /*
-Author: Arien Barley
-Created: 2nd November
-License: Public Domain
-*/
+ * Author: Arantza and Patty
+ * Created: 11/7/22
+ */
+ main();
+ var buttonEl = document.getElementById("mybutton");
+ buttonEl.addEventListener("click", function(){
+ var name = document.getElementById("form-field").value;
+ var nameEl = document.getElementById("name");
+ nameEl.innerHTML = name;
+ })
+// Thanks to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-// functions
-//utilities
-function writeToDiv(text, id){
-    //writes the passed text to the specified div
-    document.getElementById(id).innerHTML = text;
-};//end writeToDiv
-
-function arrToString(arr){
-    //returns the array in a better format for
-    //writing in html
-    s = '';
-    for (i=0;i<arr.length;i++){
-        s += arr[i].toString() + "<br>";
-    };//end for
-    return s;
-};//end arrToString
-
-function random_from(lst) {
-  // returns a random item from the passed list
-  return lst[~~(lst.length* Math.random())];
-};//end random_from
-
-function range(end, start = 0, increment = 1){
-    //returns an array incrementing as specified
-    result = [];
-    for (i=start;i<=end; i += increment){
-        result.push(i);
-    }//end for
-    return result
-}//end range
-
-function zip(...args){
-    // takes  arrays as arguments
-    //and returns an array of the form
-    //[ [arr1[0],arr2[0], arr3[0],...], [[arr1[1], arr2[1],arr3[1],...],
-    //[arr1[2], arr2[2], arr3[2],...], ..., [[arr1[n], arr2[n], arr3[n],...] ]
-    var arrays=[];
-    //turn arguments into an array
-    for (var i = 0; i<args.length; i++){
-        arrays.push(args[i]);
-    };
-
-    zipped = arrays[0].map(function(a,i){
-        var result = [a];
-        //step through arrays appending the ith element from
-        //each array to result
-        for (var j = 1; j<arrays.length; j++){
-            result.push(arrays[j][i]);
-        };
-        return result;
-    });
-
-    console.log(zipped)
-    return zipped;
-};//end zip
-
-//end utilities
-
-//name sorting function
-//functions
-function fixUserName(name, method) {
-    console.log("name = ", name);
-    //split string to an array of characters
-    var nameArray = name.split('');
-    console.log("nameArray = ", nameArray);
-
-    switch (method) {
-        case 'sort':
-            // sort nameArray
-            var fixedNameArray = nameArray.sort();
-            break;
-        case 'shuffle':
-            // thanks to ashleedawg on stack overflow for this algorithm
-            //shuffle nameArray
-            //step from the end of nameArray choosing a random
-            //to the beggining, choosing a random element from
-            //the positions before i to go into position i
-            var fixedNameArray = nameArray
-            console.log('fixed = ',fixedNameArray);
-            for(var i= fixedNameArray.length -1;i>0;i--){
-                //pick a random element from the array before
-                //index i
-                var j = Math.floor(Math.random() * (i+1));
-                //swap elements i and j in nameArray
-                var temp = fixedNameArray[i];
-                fixedNameArray[i] = fixedNameArray[j];
-                fixedNameArray[j] = temp;
-            }//end for
-
-            break;
-        default:
-            console.log("something must have gond wrong...");
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
+    return array;
+}
 
-    //turn it back into a string
-    var nameFixed = fixedNameArray.join('');
-    console.log('fixed = ',fixedNameArray);
-    return nameFixed;
-}//end fixusername
+// sortUserName - a function that takes user input and sorts the letters
+// of their name
+main2();
+function reorderUserName(word) {
+    var wordArray = word.toLowerCase().split('');
+    var newArray = shuffleArray(wordArray);
+    return newArray.join('');
+}
 
-//recurrrrrrrrrr
-function recurr(){
-    stopper = document.getElementById('depth').value;
-    //get parent div
-    var targetEl = document.getElementById('output-2');
-    add2Divs(targetEl,stopper);
-};
+// given a string, return string in Title Case
+// thanks to https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
 
+// find the button element
+buttonElAna = document.getElementById("my-button-ana");
+// find the form element
+inputElAna = document.getElementById("user-name-ana");
+// find output element
+outputElAna = document.getElementById("output-ana");
 
-// fractal div recursive function
-function add2Divs(targetEl,stopper = 1000){
-    //adds 'children' to the passed div until stopper
-    //is below 1
-    if (stopper > 1){
-        var newEl1 = document.createElement('div');
-        var newEl2 = document.createElement('div');
-        //set new element styles
-        //set left div styles based on user input
-        setStylesForRecurr( newEl1,
-                            float = 'left',
-                            border = document.getElementById('border-width').value,
-                            height = document.getElementById('height').value,
-                            width = document.getElementById('width').value,);
-        //set right div styles
-        setStylesForRecurr( newEl2,
-                            float = 'right',
-                            border = document.getElementById('border-width').value,
-                            height = document.getElementById('height').value,
-                            width = document.getElementById('width').value,
-                        );
+// add an event listener to button
+buttonElAna.addEventListener("click", function(){
+  // get value from name element
+  var userName = inputElAna.value;
+  // modify value - either sort or shuffle
+  var newName = toTitleCase(reorderUserName(userName));
+  // put value in output element
+  outputElAna.innerHTML = "<p id=name-results>" + newName + "</p>";
+      }
+  );
 
-        //add make els children of div
-        targetEl.appendChild(newEl1,overflow = 'hidden');
-        targetEl.appendChild(newEl2, float = 'right',overflow = 'hidden');
-        //recurr
-        add2Divs(newEl2,stopper/2);
-        add2Divs(newEl1,stopper/2);
-    };
+function main() {
+    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+    document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
+    var sortedName = sortUserName(userName);
+    document.writeln("Here's your sorted name: " + sortedName + "</br></br>");
+    var randomName = randomizeName(userName);
+    document.writeln("And oh hey, I fixed your name.<br>Your new one is above.");
+    document.writeln("<div class='name'>" + randomName + "</div>")
+}
 
-};//end add2Divs
-
-function setStylesForRecurr(el, float = 'left',
-                            border = 0.1,height = 40,
-                            width = 40){
-    //pick a random colour
-    var  randomcolour = '#' + Math.floor(Math.random()*16777215).toString(16);
-    //set styles to passed values
-    el.style.width = width.toString()+'%';
-    el.style.height = height.toString()+'%';
-    el.style.backgroundColor = randomcolour;
-    el.style.border = randomcolour + ' solid '+border.toString()+'px';
-    el.style.float = float;
-};//end set styles for recurr
-
-//buttons and buttons calling functions
-let resetButton = document.getElementById('reset');
-
-
-// to run the sort and  shuffle programs
-let sortButton = document.getElementById("sort");
-
-sortButton.addEventListener('click', event => {
-    //get userName
-    var name = document.getElementById("username").value;
-    //sort it
-    nameSorted = fixUserName(name,'sort');
-    //write it
-    writeToDiv("<pre>Name: " + name+
-    "\nSorted: "+nameSorted+"</pre>", "output-1");
-});
-
-let shuffleButton = document.getElementById('shuffle');
-
-shuffleButton.addEventListener('click', event => {
-    //get userName
-    var name = document.getElementById("username").value;
-    //sort it
-    nameShuffled = fixUserName(name,'shuffle');
-    //write it
-    writeToDiv( "<pre>Name: " + name + "\nShuffled: " +
-            nameShuffled + "</pre>", "output-1");
-});
+// call the function
+main2();
