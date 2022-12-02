99408534358fd42d5c171262473a304b20625d96
arantzasvz <asvilchi@ucsc.edu>
2022-12-01 16:19:50 -0800

    fine

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,18 +1,68 @@
 // Author: Arantza (asvilchi@ucsc.edu), Patty (cpieper@ucsc.edu), Anika (amittal4@ucsc,edu)
 // Created: 11/17/22
 // License: Public Domain
-document.addEventListener("DOMContentLoaded",() =>{
-	fetchCatImage()
-	btnClick()
-})
-function fetchCatImage() {
-		let image = document.getElementById ("cat-image")
-		fetch ('https://api.thecatapi.com/v1/images/search')
-		.then(resp =>resp.json())
-		.then(json =>image.src = json[0].url)
-}
+// Using the core $.ajax() method
 
-function btnClick(){
-	let button = document.getElementById ("new-cat-btn")
-	button.addEventListener("click", fetchCatImage)
-}
+ var start = "https://xkcd.com/";
+  var end = "/info.0.json";
+  var num = "";
+  var largest = 2704;
+
+  function makeURL(int){
+    //at end, back to start
+    if (int > largest){
+      int = 1;
+    }else if (int < 1) {
+      //at start, go to end
+      int = 2704;
+    }
+   console.log("new url is " + start + int.toString() + end);
+   return start + int.toString() + end;
+   }
+
+  function getAndPutData(int){
+    //clearcontent("output");
+    document.getElementById("output").innerHTML = "";
+    console.log("cleared output");
+
+    var newUrl = makeURL(int);
+    console.log("getting new api");
+
+    $.ajax({
+        url: newUrl,
+        type: "GET",
+        dataType : "json",
+        success: function(comicObj) {
+            console.log(comicObj);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error:", textStatus, errorThrown);
+        }
+    })
+
+    .done(function(comicObj) {
+      //comic
+      $("#output").append("<h3>" + comicObj.title + "<h3>");
+      $("#output").append("<img src=" + comicObj.img + ">");
+      $("#output").append("<p>" + comicObj.alt + "<p>");
+      //prev and next buttons
+      $("#output").append("<button id='prev' class='button-55'> prev </button>");
+      $("#output").append("<button id='next' class='button-55'> next </button>");
+
+      //event listner for next and prev
+      $('#next').click(function(){
+        console.log("next is clicked");
+        num = comicObj.num + 1;
+        getAndPutData(num);
+      })
+
+      $('#prev').click(function(){
+        console.log("prev is clicked");
+        num = comicObj.num - 1;
+        getAndPutData(num);
+      })
+     })
+  }
+
+ //initial comic no num
+  getAndPutData(num);
